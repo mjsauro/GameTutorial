8247a98afd0011c9875e703b61bb28d3d358e4a9
mjsauro <mjsauro@gmail.com>
2018-07-22 22:31:38 -0500

    changed dimensions, referenced bootstrap, and preparing a modal

--- js/components.js
+++ js/components.js
@@ -1,6 +1,11 @@
-	//old game piece as block
-	//myGamePiece = new component(30, 30, "blue", 10, 200);
+    //myGamePiece = new component(30, 30, "blue", 10, 200);
+var myGamePiece;
+var myBackground;
+var myObstacles = [];
+var myScore;
+    function loadComponents() {   
 	myGamePiece = new component(30, 30, "assets/spaceship.svg", 10, 120, "image");
-	myBackground = new component(656, 270, "assets/space-background.png", 0, 0, "background");
+	myBackground = new component(1000, 400, "assets/space-background.png", 0, 0, "background");
 	myObstacle = new component(10, 200, "red", 300, 120);
-	myScore = new component("30px", "Consolas", "blue", 280, 40, "text");
+    myScore = new component("30px", "Consolas", "blue", 280, 40, "text");
+}
--- js/game.js
+++ js/game.js
@@ -1,13 +1,6 @@
-var myGamePiece;
-var myBackground;
-var myObstacles = [];
-var myScore;
-
 function startGame() {
 	myGameArea.start();
 	loadComponents();
-
-
 }
 
 //game area
@@ -16,12 +9,14 @@ var myGameArea = {
 	//draw game area
 	canvas: document.createElement("canvas"),
 	start: function () {
-		this.canvas.width = 480;
-		this.canvas.height = 270;
+		this.canvas.width = 800;
+		this.canvas.height = 400;
 		this.canvas.style.cursor = "none";
 		this.context = this.canvas.getContext("2d");
 		this.frameNo = 0;
-		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		var contentDiv = document.getElementById("canvas-container");
+		//document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		contentDiv.appendChild(this.canvas);
 
 		//update the game area
 		this.interval = setInterval(updateGameArea, 20);
@@ -45,10 +40,11 @@ function everyInterval(n) {
 
 //update game every 20ms
 function updateGameArea() {
-	var x, y;
+
 	for (i = 0; i < myObstacles.length; i++) {
 		if (myGamePiece.crashWith(myObstacles[i])) {
 			myGameArea.stop();
+			alert("CRASH!");			
 			return;
 		}
 	}
